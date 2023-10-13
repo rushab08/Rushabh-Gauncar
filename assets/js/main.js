5919bb03af8d9074468c48dfae5084462c781e22
Rushabh Gauncar <rushabg988@gmail.com>
2023-10-13 17:33:18 +0530

    first commit

---
+++ assets/js/main.js
@@ -0,0 +1,5 @@
+        // Simulate a delay (e.g., loading data) before hiding the loader
+        setTimeout(function () {
+            var loader = document.getElementById("load");
+            loader.style.display = "none"; // Hide the loader
+        }, 2000); // Adjust the delay time as needed
